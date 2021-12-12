d014d70e5ba7911ae44880386ff57f923f770530
Tobias-W-S <1196184@student.roc-nijmegen.nl>
2021-12-12 19:05:37 +0100

    Javascript Randomiser part 1

--- js/index.js
+++ js/index.js
@@ -1,2 +1,13 @@
 console.log('test');
 
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+console.log(getRandomInt(3));   
+
+function test() {
+  document.getElementsByClassName("p1")[0].style.transform = "rotate(40deg)";
+}
+
+test()
